006bb7f5ea9f0c144f44e13568f52a60b9ce9149
jimbrian <ui.ux.jimbrian@gmail.com>
2022-08-03 20:59:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+function writeCards(names, event) {
+    let arrayOfThanks = [];
+    for (let i = 0; i < names.length; i++) {
+        arrayOfThanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return arrayOfThanks;
+}
+function countDown() {
+    let counter = 10;
+    while (counter >= 0) {
+        console.log(counter);
+        counter--;
+    }
+}
+
+countDown();
